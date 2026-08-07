b9e97b06cf49b937dca45069cee4f23e6e1b3a5b
daniel-sfranco <daniel.sfranco346@outlook.com>
2026-08-07 08:46:08 -0300

    Ajuste no arquivo de banco de dados

--- api/src/database.py
+++ api/src/database.py
@@ -2,7 +2,10 @@ from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = f"postgresql://{getenv('POSTGRES_USER')}:{getenv('POSTGRES_PASSWORD')}@{getenv('POSTGRES_HOST')}:{getenv('POSTGRES_PORT')}/{getenv('POSTGRES_DB')}"
+DATABASE_URL = getenv(
+    "DATABASE_URL",
+    f"postgresql://{getenv('POSTGRES_USER', 'postgres')}:{getenv('POSTGRES_PASSWORD', 'postgres')}@{getenv('POSTGRES_HOST', 'localhost')}:{getenv('POSTGRES_PORT', '5432')}/{getenv('POSTGRES_DB', 'fieldops')}"
+)
 
 engine = create_engine(DATABASE_URL)
 sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

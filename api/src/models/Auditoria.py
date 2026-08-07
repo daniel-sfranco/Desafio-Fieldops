9bfb053418ffabc54197a222b726b3794f3a921d
daniel-sfranco <daniel.sfranco346@outlook.com>
2026-08-06 21:19:23 -0300

    Criando tabela para auditoria

---
+++ api/src/models/Auditoria.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Enum, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from api.src.database import base
+
+from .OS import OS
+from .Usuario import Usuario
+from .enums.Status import Status
+
+class Auditoria(base):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
+    workOrderId: Mapped["OS"] = relationship(back_populates="auditList")
+    actorId: Mapped["Usuario"] = relationship(back_populates="auditList")
+    fromStatus: Mapped[Status] = mapped_column(Enum(Status))
+    toStatus: Mapped[Status] = mapped_column(Enum(Status))
+    createdAt: Mapped[DateTime] = mapped_column(DateTime, default=datetime.now)
